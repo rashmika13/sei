c311b0865566b1cdb8e7bd5b3bff8b597d2cf881
Jeremy Tupper <jeremytupper1@gmail.com>
2020-05-11 17:07:59 -0400

    Altered student-picker

--- w01/d4/dom-practice/js/main.js
+++ w01/d4/dom-practice/js/main.js
@@ -1,6 +1,5 @@
 const titleEl = document.getElementById('title');
 
-
 const pEl = document.querySelector('.cool');
 
 // console.log(pEl);
@@ -11,55 +10,23 @@ pEl.style.color = 'red';
 pEl.innerText = 'buckets of rain';
 
 // retrieving the innerText
-pEl.innerText // 'bucket of rain'
+pEl.innerText; // 'bucket of rain'
 console.log(pEl.innerText);
 
-pEl.innerHTML = "<h1>Buckets of Moonbeams</h1>"
-
-
-
+pEl.innerHTML = '<h1>Buckets of Moonbeams</h1>';
 
 // const aLinkGoogle = document.querySelector('a');
 // console.log(aLinkGoogle);
 
 // aLinkGoogle.setAttribute('href', 'https://www.google.com')
 
-
 // aLinkGoogle.href = 'https://www.google.com'; // totally valid and works
 
 const commentEls = document.querySelectorAll('.comment');
 console.log(commentEls);
 
-for (let comment of commentEls){
-    console.log(comment)
+for (let comment of commentEls) {
+  console.log(comment);
 
-    comment.style.fontSize = '30px';
+  comment.style.fontSize = '30px';
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
