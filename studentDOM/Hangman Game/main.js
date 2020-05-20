381953c696d51cb423975231bc07060f3e5544e5
R <50966008+rashmika13@users.noreply.github.com>
2020-05-20 10:36:01 -0400

    hangman start

---
+++ studentDOM/Hangman Game/main.js
@@ -0,0 +1,11 @@
+// create array of words
+const words= ['Maine', 'Canada' ,'Arctic','Nevada', 'Silica','Sulphur']
+// choose words randomly
+let randomNum = Math.floor(Math.random()* words.length)
+let chosenWord= word[randomNum];
+console.log(chosenWord);
+// create underscores based on lenght of word
+// Get users guess
+// check if guess is right 
+// if right push to right array
+// if wrong push to wrong array
