4a7cc80085df28a9725298a4ed9e2353f992348e
Jeremy Tupper <jeremytupper1@gmail.com>
2020-06-04 13:17:30 -0400

    In class mongoose embedding code

--- w04/d4/mongoose-embedding/instructor/mongoose-movies/routes/movies.js
+++ w04/d4/mongoose-embedding/instructor/mongoose-movies/routes/movies.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const moviesCtrl = require('../controllers/movies');
 
 router.get('/', moviesCtrl.index);
+router.get('/best', moviesCtrl.best);
 router.get('/new', moviesCtrl.new);
 router.get('/:id', moviesCtrl.show);
 router.post('/', moviesCtrl.create);
