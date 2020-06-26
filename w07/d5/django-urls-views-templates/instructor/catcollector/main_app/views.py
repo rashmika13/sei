112797556af855a6d8987c038bec1fa2f2651ccf
Denis <ddujota@internationalsafety.com>
2020-06-26 17:20:26 -0400

    finishe rendering hardcoded cats liust

--- w07/d5/django-urls-views-templates/instructor/catcollector/main_app/views.py
+++ w07/d5/django-urls-views-templates/instructor/catcollector/main_app/views.py
@@ -14,7 +14,8 @@ class Cat:  # Note that parens are optional if not inheriting from another class
 cats = [
     Cat('Lolo', 'tabby', 'foul little demon', 3),
     Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-    Cat('Raven', 'black tripod', '3 legged cat', 4)
+    Cat('Raven', 'black tripod', '3 legged cat', 4),
+    Cat("Jimmy", 'greek cat', 'mediteranian', 10)
 ]
 
 
