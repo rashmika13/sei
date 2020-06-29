66c2ca55749275498a6f6e6ee8ce716668c8f1e8
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2020-06-29 12:30:46 -0400

    Instructor code for Django models

--- w08/d1/intro-to-django-models/instructor/catcollector/main_app/views.py
+++ w08/d1/intro-to-django-models/instructor/catcollector/main_app/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
+from .models import Cat
 
-class Cat:
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-cats = [
-  Cat('Lolo', 'tabby', 'foul little demon', 3),
-  Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Cat('Raven', 'black tripod', '3 legged cat', 4)
-]
+# class Cat:
+#   def __init__(self, name, breed, description, age):
+#     self.name = name
+#     self.breed = breed
+#     self.description = description
+#     self.age = age
+#
+# cats = [
+#   Cat('Lolo', 'tabby', 'foul little demon', 3),
+#   Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+#   Cat('Raven', 'black tripod', '3 legged cat', 4)
+# ]
 
 # View functions
 
@@ -22,4 +23,11 @@ def about(request):
     return render(request, 'about.html')
 
 def cats_index(request):
+  cats = Cat.objects.all()
   return render(request, 'cats/index.html', { 'cats': cats })
+
+def cats_detail(request, cat_id):
+  # get cat from DB based on the ID in URL
+  cat = Cat.objects.get(id=cat_id)
+  # render some template
+  return render(request, 'cats/detail.html', { 'cat': cat })
